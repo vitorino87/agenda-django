496317c3092159ac92d0b9bc31b898257fcc4082
vitorino87 <brother_87_22@hotmail.com>
2022-05-12 22:59:05 -0300

    Update settings.py
    remove secret key

--- agenda/settings.py
+++ agenda/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%5bf-k7ny(j!3k9&-#0ywk8w6@)5k3_n#qy%l6wr5hce8zv3zf'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
